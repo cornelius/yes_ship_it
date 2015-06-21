64d7861c39841e2935613d2c908495fa6a915dfe
Cornelius Schumacher <schumacher@kde.org>
2015-06-22 01:14:50 +0200

    Test case where config file is not found

--- spec/integration/cli_spec.rb
+++ spec/integration/cli_spec.rb
@@ -1,9 +1,20 @@
 require_relative "spec_helper"
 
 include CliTester
+include GivenFilesystemSpecHelpers
 
 describe "command line interface" do
+  use_given_filesystem
+
+  it "does not find yes_ship_it.conf" do
+    dir = given_directory
+    expect(run_command(working_directory: dir)).to exit_with_error(1, /yes_ship_it.conf/)
+  end
+
   it "runs" do
-    expect(run_command).to exit_with_success("Shipping...\n")
+    dir = given_directory do
+      given_file("yes_ship_it.conf")
+    end
+    expect(run_command(working_directory: dir)).to exit_with_success("Shipping...\n")
   end
 end
