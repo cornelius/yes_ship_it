ed407db6cb20a7e872ef6d912a7ee0a0724fe54f
Cornelius Schumacher <schumacher@kde.org>
2015-07-02 23:39:07 +0200

    Test all assertions

--- spec/unit/assertions_spec.rb
+++ spec/unit/assertions_spec.rb
@@ -1,9 +1,11 @@
 require_relative "spec_helper"
 
-describe "assertions" do
-  describe YSI::Version do
-    it_behaves_like "an assertion" do
-      let(:assertion) { described_class.new(YSI::Engine.new) }
+describe "all assertions" do
+  [YSI::Version, YSI::ChangeLog, YSI::Tag].each do |assertion_class|
+    describe assertion_class do
+      it_behaves_like "an assertion" do
+        let(:assertion) {assertion_class.new(YSI::Engine.new) }
+      end
     end
   end
 end
