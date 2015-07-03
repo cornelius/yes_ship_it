3528d0fd094c3f10761ac6d5123384d260760d07
Cornelius Schumacher <schumacher@kde.org>
2015-07-03 17:53:01 +0200

    Add everything needed to ship yes_ship_it itself
    yes_ship_it is self-releasing now. Hooray!

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change log of yes_ship_it
+
+## Version 0.0.1
+
+* Initial release
--- assertions/built_gem.rb
+++ assertions/built_gem.rb
@@ -14,16 +14,28 @@ module YSI
       "#{@engine.project_name}-#{@engine.version}.gem"
     end
 
+    def gemspec_file
+      "#{@engine.project_name}.gemspec"
+    end
+
     def check
+      if !File.exist?(gemspec_file)
+        @error = "I need a gemspec: #{gemspec_file}"
+        return nil
+      end
+
       if !File.exist?(gem_file)
         return nil
       end
+
       gem_file
     end
 
     def assert(dry_run: false)
       if !dry_run
-        `gem build #{@engine.project_name}.gemspec`
+        if !system("gem build #{gemspec_file}")
+          return nil
+        end
       end
       gem_file
     end
