eb5274264b3c70dfc70d85f3bde4614953126d91
Cornelius Schumacher <schumacher@kde.org>
2015-08-23 18:16:35 +0200

    Parse version in Go

--- assertions/version.rb
+++ assertions/version.rb
@@ -12,17 +12,33 @@ module YSI
         return nil
       end
 
-      File.read(version_file).each_line do |line|
-        if line =~ /VERSION = "(.*)"/
-          @engine.version = $1
-          return @engine.version
-        end
+      version = parse_version(version_file)
+      if version
+        @engine.version = version
+        return @engine.version
       end
       @error = "Couldn't find version in #{version_file}"
       nil
     end
 
     def assert(dry_run: false)
+    end
+
+    def parse_version(file_name)
+      if file_name =~ /\.rb$/
+        File.read(file_name).each_line do |line|
+          if line =~ /VERSION = "(.*)"/
+            return $1
+          end
+        end
+      elsif file_name =~ /\.go$/
+        File.read(file_name).each_line do |line|
+          if line =~ /Version = "(.*)"/
+            return $1
+          end
+        end
+      end
+      nil
     end
   end
 end
--- spec/unit/assertions/version_spec.rb
+++ spec/unit/assertions/version_spec.rb
@@ -1,5 +1,7 @@
 require_relative "../spec_helper.rb"
 
+include GivenFilesystemSpecHelpers
+
 describe YSI::Version do
   it "has default path" do
     config = <<EOT
@@ -22,5 +24,21 @@ EOT
     engine.read_config(config)
 
     expect(engine.assertions.first.version_file).to eq("version.go")
+  end
+
+  describe "parses version" do
+    use_given_filesystem
+
+    let(:subject) { YSI::Version.new(YSI::Engine.new) }
+
+    it "in ruby" do
+      file = given_file("version/version.rb")
+      expect(subject.parse_version(file)).to eq("0.0.2")
+    end
+
+    it "in go" do
+      file = given_file("version/version.go")
+      expect(subject.parse_version(file)).to eq("0.0.1")
+    end
   end
 end
