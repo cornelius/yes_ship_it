0c77e5fa09912b2e650d88010c6092f3a0e8d92d
Cornelius Schumacher <schumacher@kde.org>
2015-11-29 17:06:57 +0100

    Add test for published gem

--- spec/system/ruby_gem_spec.rb
+++ spec/system/ruby_gem_spec.rb
@@ -1,7 +1,7 @@
 require_relative "spec_helper"
 
 describe "ship ruby gem" do
-  it "ships" do
+  it "builds gem" do
     out = double
     allow(out).to receive(:puts)
 
@@ -14,7 +14,7 @@ describe "ship ruby gem" do
       client.install_gem_from_spec("yes_ship_it.gemspec")
 
       remote_tar = File.expand_path("../data/red_herring-remote.tar.gz", __FILE__)
-      checkout_tar = File.expand_path("../data/red_herring-checkout.tar.gz", __FILE__)
+      checkout_tar = File.expand_path("../data/red_herring-checkout-build.tar.gz", __FILE__)
       client.inject_tarball(remote_tar)
       client.inject_tarball(checkout_tar)
 
@@ -31,6 +31,47 @@ Checking release archive: fail
 
 Asserting tag: v0.0.2
 Asserting release archive: red_herring-0.0.2.tar.gz
+
+Shipped red_herring 0.0.2. Hooray!
+EOT
+      expect(client.out).to eq(expected_output)
+
+      expect(client.err.empty?).to be(true)
+    end
+  end
+
+  it "pushes gem" do
+    out = double
+    allow(out).to receive(:puts)
+
+    test = Httpotemkin::Test.new(out: out)
+    test.add_server("rubygems")
+    test.add_server("api.rubygems")
+    test.add_server("obs")
+
+    test.run do |client|
+      client.install_gem_from_spec("yes_ship_it.gemspec")
+
+      remote_tar = File.expand_path("../data/red_herring-remote.tar.gz", __FILE__)
+      checkout_tar = File.expand_path("../data/red_herring-checkout-build.tar.gz", __FILE__)
+      client.inject_tarball(remote_tar)
+      client.inject_tarball(checkout_tar)
+
+      client.execute(["yes_ship_it.ruby2.1"], working_directory: "red_herring")
+
+      expect(client.exit_code).to eq(0)
+
+      expected_output = <<EOT
+Shipping...
+
+Checking version number: 0.0.2
+Checking tag: fail
+Checking built gem: fail
+Checking published gem: fail
+
+Asserting tag: v0.0.2
+Asserting built gem: red_herring-0.0.2.gem
+Asserting published gem: red_herring 0.0.2
 
 Shipped red_herring 0.0.2. Hooray!
 EOT
--- yes_ship_it.gemspec
+++ yes_ship_it.gemspec
@@ -16,6 +16,7 @@ Gem::Specification.new do |s|
   s.rubyforge_project         = "yes_ship_it"
 
   s.add_runtime_dependency "inifile"
+  s.add_runtime_dependency "rest-client"
 
   s.add_development_dependency "rspec", "~>3"
   s.add_development_dependency "given_filesystem"
