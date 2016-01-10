8e3edaeb65391f5561ee7d8a2ef18daccd270dc7
Cornelius Schumacher <schumacher@kde.org>
2016-01-10 19:00:11 +0100

    Prevent calling gem executable in test

--- spec/unit/assertions/published_gem_spec.rb
+++ spec/unit/assertions/published_gem_spec.rb
@@ -2,7 +2,9 @@ require_relative "../spec_helper.rb"
 
 describe YSI::PublishedGem do
   describe "#assert" do
-    it "raises AssertionError if there is an error" do
+    it "raises AssertionError if rubygems.org credentials are not there" do
+      allow(File).to receive(:expand_path).and_return("/idontexist")
+
       engine = YSI::Engine.new
       allow(engine).to receive(:project_name).and_return("IdontExist")
       allow(engine).to receive(:version).and_return("0.0")
